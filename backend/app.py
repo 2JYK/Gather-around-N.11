6e5c017b7e7b8a6b041150b66f393a918052a43d
GyeongSuKim <rla04722@naver.com>
2022-05-20 15:05:29 +0900

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask, request, jsonify, abort
 from flask_cors import CORS
 from pymongo import MongoClient
@@ -7,14 +8,47 @@ from datetime import datetime, timedelta
 app = Flask(__name__)
 cors = CORS(app, resources={r'*': {'origins': '*'}})
 client = MongoClient('localhost', 27017)
-db = client.dbsparta
+db = client.gather
 
 
-@app.route("/")
-def hello_word():
+# ㅡㅡㅡ 회원가입 ㅡㅡㅡ
+@app.route("/sub", methods = ["POST"])
+def sign_up():
+
+    data = json.loads(request.data)
+    id_receive  = data.get('id')
+    password_receive = data.get('password')
+
+# ㅡㅡㅡ 아이디 ㅡㅡㅡ
+    if id_receive == '':
+        return jsonify({'message':'아이디를 기입해 주세요'})
+    elif db.user.find_one({'id':id_receive}):
+        return jsonify({'message':'존재하는 아이디 입니다'})
+    elif len(str(id_receive)) >= 6:
+        id = id_receive
+    else:
+        return jsonify({'message':'아이디를 6자리 이상 입력해 주세요'})
     
-    return jsonify({'message':'success'})
 
+# ㅡㅡㅡ 패스워드 ㅡㅡㅡ
+    if password_receive == '':
+        return jsonify({'message':'비밀번호 입력해 주세요'})
+    elif len(str(password_receive)) >= 8:
+        password = password_receive
+    else:     
+        return jsonify({'message':'비밀번호를 8자리 이상 입력해 주세요'})
+    
+
+    password_hash = hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+# ㅡㅡㅡ db에 저장 ㅡㅡㅡ
+    doc = {
+        'ID' : id,
+        'Pw' : password_hash
+    }
+    
+    db.user.insert_one(doc)
+    return jsonify({'message':'저장완료'})    
 
 
 
