2be45252afc11ccd8a624d2d31a89ab411dd9eec
GyeongSuKim <rla04722@naver.com>
2022-05-20 15:58:44 +0900

    Feat : 회원가입 api 구현

---
+++ forntend/js/api.js
@@ -0,0 +1,20 @@
+async function signup() {
+    const signData = {
+        id : document.getElementById("floatingInput").value,
+        password : document.getElementById("floatingPassword").value
+    }
+
+    const response = await fetch("http://127.0.0.1:5000/sub", {
+        method: "POST",
+        body: JSON.stringify(signData)
+    })
+    response_json = await response.json()
+
+    if (response.status == 201) {
+        alert("환영 합니다")
+        window.location.reload();
+    } else {
+        alert("다시 확인해주세요")
+        window.location.reload();
+    }
+}
